31d603e85122bdca8f27abb88c7bdf0fe0c3e3ca
Pranav C R <pranavcr001@gmail.com>
2025-09-27 19:21:12 +0530

    nextjs typescript tailwind setup

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-blue-500">
+      <h1 className="text-4xl font-bold text-white">Tailwind is working!</h1>
+    </div>
+  )
+}

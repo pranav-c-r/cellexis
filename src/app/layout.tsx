64140023218e1570aa0bf7f116242ffa3314c9c5
Pranav C R <pranavcr001@gmail.com>
2025-09-28 19:14:31 +0530

    init builder.io

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,7 @@
 import './globals.css'
+import { builder } from '@builder.io/react';
+
+builder.init('1199fc9e321e43e1ab53708e9a0fc2f3');
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (

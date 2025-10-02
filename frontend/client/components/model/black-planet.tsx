775b224fea3a582bdd3b9239fa5e07218e60e6e2
Aditya A <adityaarun2006@gmail.com>
2025-10-02 22:43:16 +0530

    Added more 3d models

---
+++ frontend/client/components/model/black-planet.tsx
@@ -0,0 +1,38 @@
+import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls, useGLTF } from '@react-three/drei'
+import { Suspense, useRef } from 'react'
+import { Group } from 'three'
+
+function Model({ url }: { url: string }) {
+  const { scene } = useGLTF(url)
+  const modelRef = useRef<Group>(null)
+  
+  useFrame((state, delta) => {
+    if (modelRef.current) {
+      modelRef.current.rotation.y += delta * 0.1
+    }
+  })
+  
+  return (
+    <group ref={modelRef} position={[0, 0, 0]}>
+      <primitive object={scene} scale={3} rotation={[0, Math.PI/4, 0]} />
+    </group>
+  )
+}
+
+function ModelViewer() {
+  return (
+    <div className='w-full'>
+      <Canvas className='' style={{ background: 'transparent'}} camera={{ position: [5, 1, 0], fov: 75}}>
+        <ambientLight intensity={2.5} />
+        <pointLight position={[10, 10, 10]} />``
+        <Suspense fallback={null}>
+          <Model url="/models/black.glb" />
+        </Suspense>
+        <OrbitControls enableZoom={false} />
+      </Canvas>
+    </div>
+  )
+}
+
+export default ModelViewer

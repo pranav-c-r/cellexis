317c12f20a1613ae82b558d99a924cb308cf9217
vrinda-054 <vrindaraji24@gmail.com>
2025-10-05 00:13:43 +0530

    maximise graph

--- frontend/client/pages/Contact.tsx
+++ frontend/client/pages/Contact.tsx
@@ -24,6 +24,7 @@ export default function Contact() {
             });
           }}
         >
+          {/* Name, Email, Message */}
           <Input required placeholder="Name" />
           <Input required type="email" placeholder="Email" />
           <Textarea required rows={5} placeholder="Message" />
